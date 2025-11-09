8e24bebb571f5fe36de5a5c05e31e89f88f54ed2
kwasiesoko <kwasi@esoko.com>
2025-11-09 14:33:07 +0000

    added more langauges

--- src/utils/audioFile.js
+++ src/utils/audioFile.js
@@ -1,7 +1,8 @@
 let languageAudioFiles = {
-    "Ga": "https://hypertension-bucket.s3.amazonaws.com/voice-messages/english_message.mp3",
+    "Ga": "https://vbcpnjlzpfcqqfdyilzs.supabase.co/storage/v1/object/public/voice/ga.ogg",
     "English": "https://vbcpnjlzpfcqqfdyilzs.supabase.co/storage/v1/object/public/voice/english.ogg",
-    "Twi": "https://vbcpnjlzpfcqqfdyilzs.supabase.co/storage/v1/object/public/voice/twi.ogg"
+    "Twi": "https://vbcpnjlzpfcqqfdyilzs.supabase.co/storage/v1/object/public/voice/ashante.ogg",
+    "Fante": "https://vbcpnjlzpfcqqfdyilzs.supabase.co/storage/v1/object/public/voice/fante.ogg"
 };
 
 
